a5caaec493653a3183cc702e12954bab68402543
s-bromberg <shaulbromberg@gmail.com>
2025-05-14 23:23:53 -0400

    add endpoint to handle user profile pic upload

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -2,6 +2,8 @@ import express from 'express';
 import pool from '../db/pool.js';
 import multer from 'multer';
 import sessionOnly from '../validation/sessionOnly.js';
+import path from 'path';
+import { promises as fs } from 'fs';
 
 const router = express.Router();
 
@@ -24,7 +26,7 @@ const router = express.Router();
   }
 });*/
 
-router.get('/:userId', async (req, res, next) => {
+router.get('/user/:userId', async (req, res, next) => {
   try {
     const [user] = await pool.execute(
       `
@@ -47,20 +49,32 @@ router.get('/:userId', async (req, res, next) => {
   }
 });
 
+const validMimeTypes = ['image/jpeg', 'image/png', 'image/webp'];
+const validExtensions = ['.jpg', '.jpeg', '.png', '.webp'];
+
 const storage = multer.diskStorage({
-  destination: './public/images/',
+  destination: './public/images/avatars',
   filename: (req, file, cb) => {
+    console.log(file);
     const ext = file.mimetype.split('/')[1];
-    const filename =
-      `${file.fieldname}_${Date.now()}_${Math.round(Math.random() * 1E9)}.${ext}`;
+    const filename = `${Date.now()}_user${req.session.user.id}_${file.fieldname}_${Math.round(Math.random() * 1e9)}.${ext}`;
     cb(null, filename);
-  }
-})
+  },
+});
+
 const upload = multer({
   storage,
   fileFilter(req, file, cb) {
-    //check for
-    //   valid mimetype
+    const ext = path.extname(file.originalname).toLowerCase();
+    if (
+      !validMimeTypes.includes(file.mimetype) &&
+      !validExtensions.includes(ext)
+    ) {
+      const err = new Error('Only JPEG, PNG, or WebP files are allowed');
+      err.statusCode = 415;
+      return cb(err);
+    }
+
     cb(null, true);
   },
   limits: {
@@ -68,9 +82,49 @@ const upload = multer({
   },
 });
 
-router.post('/:userId/upload',/* sessionOnly,*/ upload.single('uploaded_file'), (req, res, next) => {
-  console.log(req.headers['content-type'], req.file)
-  res.end()
-});
+router.put(
+  '/upload-avatar',
+  sessionOnly,
+  upload.single('avatar'),
+  async (req, res, next) => {
+    const { id } = req.session.user;
+    console.log(id);
+    try {
+      const [result] = await pool.execute(
+        `UPDATE users
+        SET profile_image = ?
+        WHERE ID = ?`,
+        [req.file.path, id],
+      );
+
+      if (result.affectedRows === 0) {
+        throw new Error('Unable to update profile picture');
+      }
+
+      deleteOldAvatar(id);
+      res.sendStatus(204);
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  },
+);
+
+deleteOldAvatar(21);
+
+async function deleteOldAvatar(userId) {
+  const dir = 'public/images/avatars';
+  const avatars = await fs.readdir(dir);
+  const userAvatars = avatars.filter(avatar =>
+    avatar.includes(`_user${userId}`),
+  );
+  if (userAvatars.length > 1) {
+    userAvatars.sort(
+      (a, b) => Number(a.split('_')[0]) - Number(b.split('-')[0]),
+    );
+    fs.unlink(`${dir}/${userAvatars[0]}`);
+  }
+  console.log(userAvatars);
+}
 
 export default router;
